c95cc5b04630760556dcfc87bb40ceced6a86c91
cheesecake <solofox007@yandex.ru>
2021-06-25 14:12:58 +0300

    docs test

--- utils/checks.py
+++
@@ -1,8 +0,0 @@
-import utils.aiopg as aiopg
-from utils.exceptions import *
-
-
-async def checkpremium(ctx):
-    try:
-        return (await aiopg.aiogetrow("premium", [["guildid", ctx.guild.id]]))[-1]
-    except: raise NoPremiumException('no_premium')
